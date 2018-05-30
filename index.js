5d2f0b38d8df0e2d7489ed6b0c9e8d7dfe183ab2
Hanse <hkri@koren.im>
2018-05-30 20:15:42 +0200

    Ignore annoying yellow box warnings

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './app/index';
+
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader',
+  'Remote debugger is in a background tab'
+]);
+
+global.log = value => (console.log(value), value);
 
 AppRegistry.registerComponent('Mad', () => App);

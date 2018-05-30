da55934037c79710cd29e1a29d02c94436741d5a
Hanse <hkri@koren.im>
2018-05-30 16:39:34 +0200

    Add a button component

---
+++ app/components/Button.js
@@ -0,0 +1,42 @@
+// @flow
+
+import React, { Component, type Node } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+type Props = {
+  title: string,
+  style?: mixed
+};
+
+// Android should almost always use TouchableNativeFeedback for best experience
+// because it support ripples etc.
+//
+// iOS can use both TouchableHighlight and TouchableOpacity depending
+// on there the touchable is and its surroundings.
+class Button extends Component<Props> {
+  render() {
+    return (
+      <TouchableOpacity
+        {...this.props}
+        style={[styles.button, this.props.style]}
+        activeOpacity={0.9}
+      >
+        <Text style={styles.text}>{this.props.title}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    padding: 10
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20
+  }
+});
+
+export default Button;
--- app/index.js
+++ app/index.js
@@ -1,38 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// @flow
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import Button from './components/Button';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button
+          title="Make a Difference"
+          onPress={() => Alert.alert('Hello!')}
+        />
       </View>
     );
   }
@@ -43,16 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#FFF'
+  }
 });
